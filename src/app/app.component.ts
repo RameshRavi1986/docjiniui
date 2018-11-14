e2ad18db9a4a57d6e3eda32fa83e5610684918a1
Ramesh <ramtata@gmail.com>
2018-11-14 20:47:15 +0000

    Changes for latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild} from '@angular/core';
+import {Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { NavController } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
+import { ReceiptsList } from '../pages/receipts/receipts';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
-
+@ViewChild(Nav) nav: Nav;
+ rootPage:any = HomePage;
+ activePage:any;
+ receiptsRoot=ReceiptsList;
+ homeRoot = HomePage;
+ pages:Array<{title:string, component:any}>;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.pages = [
+    {title:"ReceiptsList", component:ReceiptsList},
+    {title:"Home", component:HomePage},
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +28,12 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  openPage(page){
+	this.nav.setRoot(page.component);
+	this.activePage = page;
+   }
+   checkActive(page){
+	return page === this.activePage;
+   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,74 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule  } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import {ImageParseDataService} from '../common/services/imageparser.service'
+import { RouterModule, Routes } from '@angular/router';
+import { IonicStorageModule } from '@ionic/storage';
+import { File } from '@ionic-native/file';
+import { FileOpener } from '@ionic-native/file-opener';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AppHeaderComponent} from '../common/header/d.header';
+import {AppFooterComponent} from '../common/footer/d.footer';
+import {CameraButtons} from '../common/modal/d.modal';
+import { ReceiptsList } from '../pages/receipts/receipts';
+import { ReceiptDetail } from '../pages/receiptdetails/receiptDetails';
+
+
+/*const appRoutes: Routes = [
+  { path: 'receipts-list', component: ReceiptsList },
+  { path: 'receipt/:id',   component: ReceiptDetail },
+  {
+    path: 'home',
+    component: HomePage,
+    data: { title: 'Docjini Home' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];*/
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ReceiptsList,
+    ReceiptDetail,
+    AppHeaderComponent,
+    AppFooterComponent,
+    CameraButtons
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({
+      name: '__mydb',
+         driverOrder: ['indexeddb', 'sqlite', 'websql']
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ReceiptsList,
+    ReceiptDetail,
+    CameraButtons
   ],
   providers: [
+    Camera,
+    ImageParseDataService,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    File,
+    FileOpener
   ]
 })
 export class AppModule {}
